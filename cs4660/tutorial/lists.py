d84adc2a82a4a1e89cc9787c3027bf37689d25e4
Sharokh Pournazari <sharokh.pournazari@yahoo.com>
2017-09-02 10:38:21 -0700

    Finished in calss exercise

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -2,20 +2,25 @@
 
 def get_first_item(li):
     """Return the first item from the list"""
-    pass
+    li.index(0)
 
 def get_last_item(li):
     """Return the last item from the list"""
-    pass
+    return li.index(-1)
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    pass
+    return li.index[1:2]
 
 def get_sum(li):
     """Return the sum of the list items"""
-    pass
-
+    sum = 0
+    i = 0
+    while i < len(li)
+        sum += li.index(i)
+    return sum
 def get_avg(li):
     """Returns the average of the list items"""
-    pass
+    sum = get_sum(li)
+    return sum/len(li)
+
