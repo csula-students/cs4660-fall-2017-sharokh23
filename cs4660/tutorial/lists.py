e0d7254ec2735a06b8edd973d9707375fe91d158
Sharokh Pournazari <sharokh.pournazari@yahoo.com>
2017-10-15 23:54:53 -0700

    Finally finished quiz both search methods

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,29 +1,21 @@
 """Lists defines simple list related operations"""
 
-from __future__ import division
-
 def get_first_item(li):
     """Return the first item from the list"""
-    return li[0]
+    pass
 
 def get_last_item(li):
     """Return the last item from the list"""
-    return li[-1]
+    pass
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    return li[1:3]
+    pass
 
 def get_sum(li):
     """Return the sum of the list items"""
-    return sum(li)
-    """
-    sum = 0
-    for i in li:
-        sum += i
-    retrun sum
-    """
+    pass
 
 def get_avg(li):
     """Returns the average of the list items"""
-    return get_sum(li) / len(li)
+    pass
